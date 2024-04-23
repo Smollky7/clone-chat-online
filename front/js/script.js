d5517deca8c33216bf3f1c3332eb6024846e2343
Snollky7 <jarbasrt81@gmail.com>
2024-04-23 20:45:12 -0300

    update script.js

--- front/js/script.js
+++ front/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://clone-chat-online.onrender.com")
     websocket.onmessage = processMessage
 }
 
